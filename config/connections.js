3f05ec2866ba9359dc1f7e45383ae59497aee406
Gelo <amjavonitalla@gmail.com>
2015-11-26 23:37:44 +0800

    Adding config for mongolab

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,9 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongolab: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    url: process.env.MONGOLAB_URI
   },
 
   /***************************************************************************
